743759628a0575b7e2595f283feedb69ed261aa2
James <jimmyheno06@gmail.com>
2026-05-24 21:13:38 +1000

    finished process_eoi, however needs testing

--- process_eoi.php
+++ process_eoi.php
@@ -3,6 +3,24 @@ require_once 'settings.php';
     session_start();
     $errors = array();
     $conn = mysqli_connect($host, $user, $pwd, $sql_db);
+    $create = "CREATE TABLE IF NOT EXISTS EOI (
+    EOInumber int(11) NOT NULL AUTO_INCREMENT,
+    job_reference varchar(5) NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    dob date NOT NULL,
+    gender varchar(20) NOT NULL,
+    email varchar(100) NOT NULL,
+    phone varchar(12) NOT NULL,
+    address varchar(40) NOT NULL,
+    suburbtown varchar(40) NOT NULL,
+    state varchar(4) NOT NULL,
+    postcode varchar(4) NOT NULL,
+    other_skills text DEFAULT NULL,
+    status enum('NEW','CURRENT','FINAL') NOT NULL DEFAULT 'NEW',
+    PRIMARY KEY (EOInumber)
+)";
+mysqli_query($conn, $create);
     function sanitise_input($data, $conn) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -34,13 +52,46 @@ require_once 'settings.php';
             $errors[]= "Please enter a valid last name";
         }
         $dob = $_POST["dob"] ?? null;
+        if(empty($dob)){
+            $errors[]= "Please enter your date of birth";
+        }
         $gender = sanitise_input($_POST["gender"], $conn);
+        if(empty($gender)){
+            $errors[]= "Please select a gender";
+        }
         $email = sanitise_input($_POST["email"], $conn);
+        if(empty($email)){
+            $errors[]= "Please enter your email";
+        }
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "Please enter a valid email address";
+        }
         $phone = sanitise_input($_POST["phone"], $conn);
+        if(empty($phone)){
+            $errors[]= "Please enter your phone number";
+        }
+        if(!preg_match("/^[0-9]{8,12}$/", $phone)){
+            $errors[]= "Please enter a valid phone number";
+        }
         $address = sanitise_input($_POST["address"], $conn);
+        if(empty($address)){
+            $errors[]= "Please enter your address";
+        }
         $suburbtown = sanitise_input($_POST["suburbtown"], $conn);
+        if(empty($suburbtown)){
+            $errors[]= "Please enter your suburb/town";
+        }
         $state = sanitise_input($_POST["state"], $conn);
+        if(empty($state)){
+            $errors[]= "Please enter your state";
+        }
         $postcode = sanitise_input($_POST["postcode"], $conn);
+        if(empty($postcode)){
+            $errors[]= "Please enter your postcode";
+        }
+        if(!preg_match("/^[0-9]{4}$/", $postcode)){
+            $errors[]= "Please enter a valid phone number";
+        }
         $communication = isset($_POST["communication"]) ? 1 : 0;
         $problem_solving = isset($_POST["problem_solving"]) ? 1 : 0;
         $leadership = isset($_POST["leadership"]) ? 1 : 0;
@@ -53,7 +104,30 @@ require_once 'settings.php';
         $project_management = isset($_POST["project_management"]) ? 1 : 0;
         $critical_thinking = isset($_POST["critical_thinking"]) ? 1 : 0;
         $attention_to_detail = isset($_POST["attention_to_detail"]) ? 1 : 0;
+        if(($communication + $problem_solving + $leadership + $technical + $time_management + $teamwork + $adaptability + $data_analysis + $customer_service + $project_management + $critical_thinking + $attention_to_detail) == 0){
+            $errors[] = "Please select at least one skill";
+        }
         $other_skills = sanitise_input($_POST["other_skills"], $conn);
+    
+        if(!empty($errors)) {
+            foreach($errors as $error) {
+                echo "<p style='color:red;'>$error</p>";
+        }
+        } else {
+            $query = "INSERT INTO EOI (job_reference, job_description, first_name, last_name, dob, gender, email, phone, address, suburbtown, state, 
+            postcode, communication, problem_solving, leadership, technical, time_management, teamwork, adaptability, data_analysis, customer_service,
+             project_management, critical_thinking, attention_to_detail, other_skills) VALUES ('$job_reference', '$job_description', '$first_name', '$last_name', '$dob',
+              '$gender', '$email', '$phone', '$address', '$suburbtown', '$state', '$postcode', '$communication', '$problem_solving', '$leadership', '$technical', '$time_management',
+               '$teamwork', '$adaptability', '$data_analysis', '$customer_service', '$project_management', '$critical_thinking', '$attention_to_detail', '$other_skills')";
+            $result = mysqli_query($conn, $query);
+        }
+        if ($result) {
+            $eoi_number = mysqli_insert_id($conn);
+            echo "<p>Application submitted successfully! Your EOI number is: <strong>$eoi_number</strong></p>";
+        } else {
+            echo "<p>Error submitting application. Please try again.</p>";
+        }
+
     }
     else{
     header('Location: index.php');
